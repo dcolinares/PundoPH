5600477a96928a40f01507437e574808d1d6a6b9
dcolinares <d.colinares@outlook.com>
2025-02-05 15:56:24 +0800

    Update dll

--- PundoPH/PundoPH/obj/Debug/net6.0/PundoPH.AssemblyInfo.cs
+++ PundoPH/PundoPH/obj/Debug/net6.0/PundoPH.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PundoPH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+802e6d68a10b59d8cf46f95c36f578b63db230be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1d9dda8aaf27e8751e83bc8ec14a04fa3d16c1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PundoPH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PundoPH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
