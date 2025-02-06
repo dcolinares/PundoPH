ea2f7420e7f5b6b10ec1466a0485c2c7fbc140fa
dcolinares <d.colinares@outlook.com>
2025-02-06 18:08:19 +0800

    Add download to pdf

--- PundoPH/PundoPH/Data/ExportService.cs
+++ PundoPH/PundoPH/Data/ExportService.cs
@@ -1,8 +1,10 @@
-﻿using DocumentFormat.OpenXml.Spreadsheet;
+﻿
+using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using PundoPH.Model;
-using System.IO;
 using static System.Net.WebRequestMethods;
+using System.Reflection.Metadata;
+using System;
 
 namespace PundoPH.Data
 {
@@ -17,14 +19,33 @@ namespace PundoPH.Data
             _JSRuntime = jSRuntime;
         }
 
-        public async Task DownloadExcel(string fileName, List<Contribution> contributions)
+        public async Task Excel(string fileName, List<Contribution> contributions)
         {
-            var response = await _http.PostAsJsonAsync("api/excel/export", contributions);
+            var response = await _http.PostAsJsonAsync($"api/download/excel?fileName={fileName}", contributions);
             if (response.IsSuccessStatusCode)
             {
+                var fileType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
                 var fileBytes = await response.Content.ReadAsByteArrayAsync();
                 using var streamRef = new DotNetStreamReference(new MemoryStream(fileBytes));
-                await _JSRuntime.InvokeVoidAsync("downloadFile", fileName, streamRef);
+                await _JSRuntime.InvokeVoidAsync("downloadFile", fileName, streamRef, fileType);
+            } else
+            {
+                Console.WriteLine("Failed to fetch excel.");
+            }
+        }
+
+        public async Task Pdf(List<Contribution> contributions, string fileName)
+        {
+            var response = await _http.PostAsJsonAsync($"api/download/pdf?fileName={fileName}", contributions);
+            if (response.IsSuccessStatusCode)
+            {
+                var fileType = "application/pdf";
+                var fileBytes = await response.Content.ReadAsByteArrayAsync();
+                using var streamRef = new DotNetStreamReference(new MemoryStream(fileBytes));
+                await _JSRuntime.InvokeVoidAsync("downloadFile", fileName, streamRef, fileType);
+            } else
+            {
+                Console.WriteLine("Failed to fetch PDF.");
             }
         }
     }
--- PundoPH/PundoPH/obj/Debug/net6.0/PundoPH.AssemblyInfo.cs
+++ PundoPH/PundoPH/obj/Debug/net6.0/PundoPH.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PundoPH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5600477a96928a40f01507437e574808d1d6a6b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e181a7a703f60b267d27829153ceee1012884ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("PundoPH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PundoPH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
