27d8eff586788d89f024f0cb98d5c2398fe00b18
dcolinares <d.colinares@outlook.com>
2025-02-07 19:08:22 +0800

    remove fetching token in prerender

--- PundoPH/PundoPH/obj/Debug/net6.0/PundoPH.AssemblyInfo.cs
+++ PundoPH/PundoPH/obj/Debug/net6.0/PundoPH.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PundoPH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d52156746887a364921552d84dfeb21f90f0b53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13fe8c46ef3277c5c8929f05adba397ec30307ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("PundoPH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PundoPH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
